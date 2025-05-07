f8914c9bd24b404d96930a3f0dffef1f2e785262
Lucian Regus <lucian.accounts@proton.me>
2025-05-07 18:28:27 +0300

    feat: change paths

--- domain/include/domain/config.h
+++ domain/include/domain/config.h
@@ -5,6 +5,6 @@
 #define FANOTIFY_BUFFER_SIZE 1024
 #define FANOTIFY_THREAD_POOL_SIZE 10
 
-#define PLUGIN_DIR "/usr/lib/voodoo_av/plugins"
-#define QUARANTINE_DIR "/var/lib/voodoo_av/quarantine/"
-#define LOG_FILE "/var/log/voodoo_av/voodoo_engine.log"
+#define PLUGIN_DIR "/usr/lib/voodoo/plugins"
+#define QUARANTINE_DIR "/var/lib/voodoo/quarantine/"
+#define LOG_FILE "/var/log/voodoo/voodoo_engine.log"
